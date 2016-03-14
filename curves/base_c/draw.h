f83e09274bc0acea0e3fa476773268c5923d4f9e
JonAlf <jonalf@gmail.com>
2016-03-14 10:42:10 -0400

    fixed draw.h

--- curves/base_c/draw.h
+++ curves/base_c/draw.h
@@ -6,6 +6,8 @@
 void draw_line(int x0, int y0, 
 	       int x1, int y1, 
 	       screen s, color c);
+void add_point( struct matrix * points, 
+		double x0, double y0, double z0); 
 void add_edge( struct matrix * points, 
 	       double x0, double y0, double z0, 
 	       double x1, double y1, double z1);
